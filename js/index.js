768db067248e53973968d34d08fb3b1e9f09066d
Kim schenk <k.schenk@belastingdienst.nl>
2021-02-14 21:40:43 +0100

    click container

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+// function showKarakter() {
+//   document.getElementById("karakter_container").style.visibility = "visible";
+// }
+
+// function showKarakter() {
+//     var currentState = document.getElementById("karakter_container").style.visibility = "hidden";
+//     var state = currentState ? "visible":"hidden";
+//     document.getElementById("karakter_container").style.visibility  = state;
+// }
+
 function showKarakter() {
-  document.getElementById("karakter_container").style.visibility = "visible";
+  var state = document.getElementById(
+    "karakter_container");
+    state.style.visibility = state.style.visibility === "hidden" ? "visible" : "hidden";
 }

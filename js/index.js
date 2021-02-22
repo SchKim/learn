f0af8c6e792966d1225268a2c3acfc54f7bee5cb
Kim schenk <k.schenk@belastingdienst.nl>
2021-02-22 13:13:13 +0100

    BEM added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function showKarakter() {
   var state = document.getElementsByClassName(
-    "karakter");
+    "karakter__revealer");
     var aNode = state[0];
     aNode.style.visibility = aNode.style.visibility === "hidden" ? "visible" : "hidden";
 }

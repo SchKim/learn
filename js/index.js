df859633888c6e83f699e431bdfdacbacd8615a8
Kim schenk <k.schenk@belastingdienst.nl>
2021-04-10 23:31:31 +0200

    start display items in style

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // const URL = "./assets/api.json";
 
+// const bla = new Request("../assets/api.json");
+
 // const fetchCharacter = () => {
-//   fetch("../assets/api.json")
+//   fetch(bla)
 //     .then((res) => res.json())
 //     .then((data) => console.log(data))
 //     .catch((error) => {
@@ -17,87 +19,83 @@ let CharacterJson = [
     title: "Wie ben ik ?",
     description:
       "Ik ben een enthousiaste HBO opgeleide Front-End Developer en Scrum master. Met een passie voor innovatie, wil altijd op de hoogte zijn van de laatste trends. ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-smile-wink",
   },
   {
     id: 2,
     title: "Persoonlijke eigenschappen",
     description:
       "Ik ben een echt mensen mens, heb een gevoel voor humor. Ben bijna altijd vrolijk. Collega's omschrijven mij als gezellig, gedreven, doorzettingsvermogen, creatief, communicatief, analytisch en beschik over een probleemoplossend vermogen. ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-user-tag",
   },
   {
     id: 3,
     title: "Team",
     description:
       "Ik Krijg energie van het creëren van coole dingen met andere geeks. Het liefst werk ik samen met andere ontwikkelaars en draag bij aan een goede sfeer binnen het team.  ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-users",
   },
   {
     id: 4,
     title: "Development stack",
     description:
       "Ik ben een enthousiast aan het ontwikkelen met HTML, CSS, JS, TS, React en Styled components ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-code",
   },
   {
     id: 5,
     title: "Ambities",
     description:
       "Mijn ambitie is om een goede senior Front-end developer te worden die het team CoDE goed kan begeleiden en ondersteunen op alle fronten ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-user-cog",
   },
   {
     id: 6,
-    title: "Hobby's",
+    title: "Vrije tijd",
     description:
-      "Uiteraard is ontwikkellen een een grote hobby daarnaast heb ik nog een paard en een huis van 100 jaar oud waar het nodige aan moet gebeuren ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+      "De meeste vrije tijd breng ik met mijn lieve dochter Lenthè door. Uiteraard is ontwikkellen een een grote hobby daarnaast heb ik nog een paard en een huis van 100 jaar oud waar het nodige aan moet gebeuren. Daar gaat de rest van de tijd inzitten",
+    symbol: "fas fa-battery-three-quarters",
   },
   {
     id: 7,
     title: "Interest",
     description:
       "Mijn interesses liggen bij  HTML, CSS, JS, TS, React en Styled components daar wil ik mij eerst om focussen en daarna kijk ik verder",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-glasses",
   },
   {
     id: 8,
     title: "Signature",
     description:
       "Wat absoluut kenmerkend aan mij is, is dat ik altijd een goede sfeer in het team probeer te behouden, mensen samen breng en iedereen probeer te helpen waar ik kan.",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-signature",
   },
   {
     id: 9,
     title: "Specialties",
     description:
       "Ja dat vind ik altijd een beetje lastig om over mijzelf te zeggen. Mensen blijven graag eten als ik kook. Ben goed in het organiseren van dingen. ",
-    symbol: "https://rickandmortyapi.com/api/location/1",
+    symbol: "fas fa-grin-stars",
   },
 ];
 
-let characters = CharacterJson;
-console.log(characters);
-
 const iconKarakter = (characters) => {
-  const characterDiv = document.querySelector(".karakter__icon_wrapper");
+  const characterDiv = document.querySelector("#karakter__icon_wrapper");
   characters.forEach((character) => {
     const characterTitle = document.createElement("h1");
     const characterDiscription = document.createElement("p");
     const characterIcon = document.createElement("i");
-
-    characterTitle.innerHTML = `Titele: ${character.title}`;
-    characterDiscription.innerText = `Omschrijving: € ${character.description}`;
+    characterTitle.innerText = character.title;
+    characterDiscription.innerText = character.description;
     characterIcon.setAttribute("class", `${character.symbol}`);
-
     characterDiv.append(characterTitle);
     characterDiv.append(characterDiscription);
     characterDiv.append(characterIcon);
   });
 };
-// iconKarakter();
-console.log(CharacterJson[0].title);
+
+const characters = CharacterJson;
+iconKarakter(characters);
 
 function showKarakter() {
   var state = document.getElementsByClassName("karakter__revealer");

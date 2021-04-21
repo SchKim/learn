cfb095361f6c5884645068b9e8893b80675adb5f
Kim schenk <k.schenk@belastingdienst.nl>
2021-04-21 15:41:33 +0200

    function main works

--- js/index.js
+++ js/index.js
@@ -84,53 +84,86 @@ const iconKarakter = (characters) => {
     const characterDiv = document.createElement("div");
     const characterButton = document.createElement("button");
     const characterIcon = document.createElement("i");
-
     const characterTitle = document.createElement("h1");
     const characterDiscription = document.createElement("p");
 
     characterButton.classList.add("characterBtn");
+    // characterButton.addEventListener(
+    //   "click",
+    //   (event) => showMore(character.description, textDiv) // ik kan hier het carajter id meegeven
 
     characterButton.addEventListener("click", (event) =>
-      showMore(character.description, textDiv)
+      main(character.description, textDiv, character.id)
     );
-    characterIcon.setAttribute("class", `${character.symbol}`); // classlist maken
-
+    characterIcon.setAttribute("class", `${character.symbol}`);
     characterTitle.classList.add("characterTitle_moreInfo");
     characterTitle.innerText = character.title;
-
     characterDiscription.classList.add("characterDiscription_moreInfo");
-
     characterDivID.appendChild(characterDiv);
     characterDiv.appendChild(characterButton);
     characterButton.appendChild(characterIcon);
   });
 };
 
-function showMore(text, targetdiv) {
-  const putInArray = text.split("");
-  console.log(putInArray);
-  loopText(putInArray, targetdiv);
+function showMore(texts) {
+  const splitText = texts.split("");
+  return splitText;
 }
+
 //in var opslaan hoevell keer i text aan het lopen en wlke knop
-//
 //hoeveel keer looptext
 // clear time out triggeren al je wilt stoppen ]
 // als er een text aan het lopee is is de text van dezelede knop aan het lopen doeniks. i het een andere knp willes text
-function loopText(putInArray, show) {
-  let timer = 0;
-  if (putInArray) {
-    if (putInArray.length > 0) {
-      show.innerHTML += putInArray.shift();
+let timer = 0;
+
+function loopText(textLoss, show) {
+  if (textLoss) {
+    if (textLoss.length > 0) {
+      show.innerHTML += textLoss.shift();
     } else {
-      clearTimeout(timer);
+      console.log(timer);
+      stopTimer(timer);
       return false;
     }
     timer = setTimeout(function () {
-      loopText(putInArray, show);
+      loopText(textLoss, show);
     }, 70);
   }
 }
+//waar moet dit staan met die higher order dinges
+
+function stopTimer() {
+  clearTimeout(timer);
+}
+function main(text, targetDiv, id) {
+  console.log(text);
+  // console.log(targetDiv);
+  // console.log(id);
+
+  const last = id;
+  // console.log(id);
+  // console.log(id);
+  // console.log(last != last);
+  // if (last == last) {
+  //   console.log("ik ben if");
+  //   stopTimer(timer);
+  //   return false;
+  // } else {
+  //   console.log("ik ben else");
+  showMore(text);
+  const gesplitteText = showMore(text); // ??????? waarom moet ik text als parameter hier mee geven ?? shormore heeft toch mij uitput ?
+  console.log(gesplitteText);
+  //uitkomst shormmore in loop text & looptext uit show more halen
+  loopText(gesplitteText, targetDiv);
+}
+//}
+
 // looptext niet herhandelijk aanroepen vanuit loop text maar roept andere functie aan
 //inner functie in set loopt ext timer buiten loopt text als timer niet 0 is
-// high order stuv=xtuur
+// high order stuctuur
 iconKarakter(characters);
+
+//main krijgt 2 aggumenten binnen text , targetdiv
+// text geeft die mee aam showmore daar de uitkomst van
+// mpet worden gebruikt als para meter voor loop text + de div
+// dit voor komt
